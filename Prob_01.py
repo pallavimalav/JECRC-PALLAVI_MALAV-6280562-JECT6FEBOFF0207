30a1c28699822200934db02f939ae4b8a59223b7
Pallavi Malav <mpallavi1803@gmail.com>
2026-03-01 19:08:50 +0530

    Delete Prob_01.py

--- Prob_01.py
+++
@@ -1,9 +0,0 @@
-# WAP to check whether a yaer is leap year or not.
-year = int(input("Enter a year: "))
-
-if (year % 4 == 0 and year % 100 != 0):
-    print(year, "is a Leap Year")
-else:
-    print(year, "is NOT a Leap Year")
-
-    
